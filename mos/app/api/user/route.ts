6bce9ac8bec1c42636f3ba2bf1167ee3a43f5800
ziad-kamel <ziad,mohamed.kamell.184@gmail.com>
2026-01-10 19:17:37 +0200

    configure prisma

---
+++ mos/app/api/user/route.ts
@@ -0,0 +1,38 @@
+import prisma from "@/lib/prisma"
+
+
+export async function GET() {
+  const users = await prisma.user.findMany()
+  return Response.json(users)
+}
+
+export async function POST(request: Request) {
+  const { name, email } = await request.json()
+  const newUser = await prisma.user.create({
+    data: {
+      name,
+      email,
+    },
+  })
+  return Response.json(newUser)
+}
+
+export async function PUT(request: Request) {
+  const { id, name, email } = await request.json()
+  const updatedUser = await prisma.user.update({
+    where: { id },
+    data: {
+      name,
+      email,
+    },
+  })
+  return Response.json(updatedUser)
+}
+
+export async function DELETE(request: Request) {
+  const { id } = await request.json()
+  const deletedUser = await prisma.user.delete({
+    where: { id },
+  })
+  return Response.json(deletedUser)
+}   
