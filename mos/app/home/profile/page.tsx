e50f8b2a93cd901cd9c077538c2c4765bc466c11
ziad-kamel <ziad,mohamed.kamell.184@gmail.com>
2026-03-19 17:55:31 +0200

    debug with new profile page

---
+++ mos/app/home/profile/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import ProfileForm from "@/components/profile-form";
+import {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+  BreadcrumbPage,
+} from "@/components/ui/breadcrumb";
+import { useUser } from "@/providers/user-provider";
+
+export default function Profile() {
+  return (
+    <div className='flex flex-col min-h-svh w-full items-center justify-center p-6 md:p-10'>
+      <div className='flex w-full'>
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href='/home'>Home</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>Profile</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+      <ProfileForm />
+    </div>
+  );
+}
