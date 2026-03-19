bb83493a1f0c6cd609e7107099bc34030108d937
ziad-kamel <ziad,mohamed.kamell.184@gmail.com>
2026-03-19 15:49:17 +0200

    debug proxy error

--- mos/proxy.ts
+++ mos/proxy.ts
@@ -21,6 +21,6 @@ export const config = {
     // "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
     "/",
     "/register",
-    "/auth/signout",
+    // "/auth/signout",
   ],
 };
