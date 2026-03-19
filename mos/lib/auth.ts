d25db33e08daada18c74f965523ce179c112a8bc
ziad-kamel <ziad,mohamed.kamell.184@gmail.com>
2026-03-19 15:04:47 +0200

    debug auth error

--- mos/lib/auth.ts
+++ mos/lib/auth.ts
@@ -8,10 +8,14 @@ export const authUser = async (req: NextRequest) => {
 
   if (!error) {
     if (data) {
+      console.log("authed here", data);
+
       //user authed
       //TODO: take the req.cookies.token and decode it with JWT then compare the calims from the token with the data from supabase claims
       return NextResponse.next();
     } else {
+      console.log("not authed here", data);
+
       return NextResponse.redirect(`${baseURL}/login`);
     }
   } else {
